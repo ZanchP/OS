31b4bdfa6f0076c906f254c5679ff1c6ff3ed9b1
ZanchP <39904602+ZanchP@users.noreply.github.com>
2020-10-16 23:08:01 +0200

    Add files via upload

---
+++ naloga (1).sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if test $# -eq 1; then	#Test če je podan samo 1 argument
+	cd $(realpath $1)	#Prestavi se v podan direktorij z absolutno potjo 		
+	for i in $(ls -l); do	#For zanka čez vse elemente v direktorju
+  		if [ -f $i ]; then	#Test če je i-ti element tipa "file"
+       		dat=$(ls $i | grep .txt)	#Ustvari spremenljivko "file" i-tega elementa z končnico .txt
+        	       	if [ $dat ]; then	#Če ta obstaja
+               			cp $dat $dat.old	#Kopiraj datoteko in dodaj kratico .old
+               		fi
+       		fi
+	done
+else
+	echo "Skripta sprejme samo 1 argument"	#Izpis če je podan več kot 1 argument 
+fi
